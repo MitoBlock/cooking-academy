f4815ca522dd6314005a08328cbcc770da64ab32
alogjedi <vicbanwait@hotmail.com>
2022-10-31 14:35:30 -0400

    removed third page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-import { ThirdPageComponent } from './third-page/third-page.component';
 import { UserPageComponent } from './user-page/user-page.component';
 
 const appRoutes: Routes = [
   { path: '', component: MainPageComponent },
   { path: 'user', component: UserPageComponent },
-  { path: 'user/offers', component: ThirdPageComponent },
 ];
 
 @NgModule({
